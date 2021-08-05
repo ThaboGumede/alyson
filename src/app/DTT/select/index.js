05a34d397adb8bc298a098398e227e9b582f9975
c7yrus <42728641+c7yrus@users.noreply.github.com>
2021-08-05 11:38:23 +1000

    dd feature enhancement for dropdown

--- src/app/DTT/select/index.js
+++ src/app/DTT/select/index.js
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from 'react'
 import { compose, includes, map, pathOr } from 'ramda'
 import { useSelector } from 'react-redux'
 import { Text, Select as CSelect } from '@chakra-ui/react'
@@ -29,6 +30,12 @@ const Write = ({
   const options = compose(map(({ code, name }) => ({ label: name, value: code })))(dropdownData)
 
   const { attributeCode } = data || {}
+
+  const allOptions = useRef([])
+
+  useEffect(() => {
+    allOptions.current = [...options, ...allOptions.current]
+  })
 
   const ddEvent = debounce(
     value =>
@@ -65,6 +72,7 @@ const Write = ({
     <Autocomplete
       placeholder={!options.length ? 'Start typing to search' : placeholder || 'Select'}
       options={options}
+      allOptions={allOptions.current}
       onChange={onSendAnswer}
       defaultValue={defaultValue}
       multiple={multiple}
